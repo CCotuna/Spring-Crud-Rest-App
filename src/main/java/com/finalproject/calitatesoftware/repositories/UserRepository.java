22a23ef899634ffdb3b8970ff1c3dec8aa8b2569
Calin <cotuna.calin@yahoo.com>
2025-05-02 07:41:01 +0300

    add missing @Repository annotation

--- src/main/java/com/finalproject/calitatesoftware/repositories/UserRepository.java
+++ src/main/java/com/finalproject/calitatesoftware/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.finalproject.calitatesoftware.repositories;
 
 import com.finalproject.calitatesoftware.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
